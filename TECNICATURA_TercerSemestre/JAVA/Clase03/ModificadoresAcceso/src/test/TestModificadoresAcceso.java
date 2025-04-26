b12c79d4bb7bf24dab351cca9609d645d21c8501
Mariana Aguilera <marian5174@gmail.com>
2025-04-26 00:47:28 -0300

    Modificadores de acceso Protected

--- TECNICATURA_TercerSemestre/JAVA/Clase03/ModificadoresAcceso/src/test/TestModificadoresAcceso.java
+++ TECNICATURA_TercerSemestre/JAVA/Clase03/ModificadoresAcceso/src/test/TestModificadoresAcceso.java
@@ -2,6 +2,7 @@
 package test;
 
 import paquete1.Clase1;
+import paquete2.Clase3;
 
 
 public class TestModificadoresAcceso {
@@ -9,6 +10,8 @@ public class TestModificadoresAcceso {
         Clase1 clase1 = new Clase1();
         System.out.println("clase1 = "+ clase1.atributoPublic);
         clase1.metodoPublico();
+        Clase3 claseHija = new Clase3();
+        System.out.println("claseHija = "+ claseHija);
     }
     
 }
