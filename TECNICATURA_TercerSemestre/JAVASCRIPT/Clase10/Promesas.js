3a7d691a8efe2dd4b9f5047ad4d07b7bfc8fa29a
Mariana Aguilera <marian5174@gmail.com>
2025-06-10 21:47:44 -0300

    Función setTimeout

--- TECNICATURA_TercerSemestre/JAVASCRIPT/Clase10/Promesas.js
+++ TECNICATURA_TercerSemestre/JAVASCRIPT/Clase10/Promesas.js
@@ -1,5 +1,5 @@
 let miPromesa = new Promise( (resolver, rechazar) => { //creamos el objeto de tipo promesa, con dos parámetros callback
-    let expresion = false;
+    let expresion = true;
     if(expresion){
         resolver('Resolvió correctamente'); //cuando termina bien/si es verdadera la expresión
     }else {
@@ -12,6 +12,15 @@ let miPromesa = new Promise( (resolver, rechazar) => { //creamos el objeto de ti
    // error => console.log(error) //función callback para cuando se rechaza
 //);
 
-miPromesa
-    .then( valor => console.log(valor))
-    .catch(error => console.log(error));
+//miPromesa
+//    .then( valor => console.log(valor))
+//    .catch(error => console.log(error));
+
+let promesa = new Promise((Resolver) => {
+    console.log('Inicio promesa');
+    setTimeout( () => Resolver('Saludos desde promesa, callback, función flecha y setTimeout'),3000)
+    console.log('Final promesa');
+});
+
+//llamado a la promesa
+promesa.then( valor => console.log(valor));
