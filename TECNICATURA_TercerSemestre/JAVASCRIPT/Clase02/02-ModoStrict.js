78dd78fff77e3a97a9a4305edfb3309a09c6f72c
ezequiel <quirozarielezequiel@gmail.com>
2025-06-23 19:08:20 -0300

    Comento la clase02 de JS

--- TECNICATURA_TercerSemestre/JAVASCRIPT/Clase02/02-ModoStrict.js
+++ TECNICATURA_TercerSemestre/JAVASCRIPT/Clase02/02-ModoStrict.js
@@ -1,12 +1,13 @@
-"use strict";
+"use strict"; // Activa el modo estricto de JavaScript a nivel global, lo que ayuda a escribir un código más seguro y limpio
 
-let x = 10;
-console.log(x);
-
+let x = 10; // Declara una variable 'x' con valor 10
+console.log(x); // Muestra el valor de 'x' en la consola
 
 function miFuncion() {
-    "use strict";
-    let y = 10;
-    console.log(y);
+    "use strict"; // Activa el modo estricto dentro de la función (redundante si ya está activado globalmente)
+
+    let y = 10; // Declara una variable 'y' con valor 10 dentro del ámbito de la función
+    console.log(y); // Muestra el valor de 'y' en la consola
 }
-miFuncion();
+
+miFuncion(); // Llama a la función 'miFuncion' para ejecutar su contenido
--- TECNICATURA_TercerSemestre/JAVASCRIPT/Clase02/02-POO.js
+++ TECNICATURA_TercerSemestre/JAVASCRIPT/Clase02/02-POO.js
@@ -1,29 +1,32 @@
 class Empleado {
+    // Constructor de la clase Empleado que recibe nombre y sueldo
     constructor(nombre, sueldo){
-        this._nombre = nombre;
-        this._sueldo = sueldo;
+        this._nombre = nombre;  // Asigna el nombre al atributo privado _nombre
+        this._sueldo = sueldo;  // Asigna el sueldo al atributo privado _sueldo
     }
 
+    // Método para obtener los detalles del empleado como string
     obtenerDetalles(){
         return `Empleado: nombre: ${this._nombre},
                 sueldo: ${this._sueldo}`;
     }    
 }
 
-class Gerente extends Empleado{
+class Gerente extends Empleado {
+    // Constructor de la clase Gerente que hereda de Empleado y agrega departamento
     constructor(nombre, sueldo, departamento){
-        super(nombre, sueldo);
-        this._departamento = departamento;
+        super(nombre, sueldo);  // Llama al constructor de la clase padre para nombre y sueldo
+        this._departamento = departamento;  // Asigna el departamento al atributo privado _departamento
     }
 
-    //Agregamos la sobreescritura
+    // Sobrescribe el método obtenerDetalles para incluir el departamento
     obtenerDetalles(){
         return `Gerente: ${super.obtenerDetalles()} depto: ${this._departamento}`;
     }
 }
 
-let gerente1 = new Gerente("Carlos", 5000, "Sistemas");
-console.log(gerente1); //Objeto de la clase hija
+let gerente1 = new Gerente("Carlos", 5000, "Sistemas"); // Crea un objeto Gerente con nombre, sueldo y departamento
+console.log(gerente1); // Muestra en consola el objeto Gerente (incluye sus propiedades)
 
-let empleado1 = new Empleado("Juan", 3000);
-console.log(empleado1) //Objeto de la clase padre
+let empleado1 = new Empleado("Juan", 3000); // Crea un objeto Empleado con nombre y sueldo
+console.log(empleado1); // Muestra en consola el objeto Empleado
