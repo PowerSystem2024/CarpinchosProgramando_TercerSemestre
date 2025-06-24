eac7cb32e2f9426b2d0699b028ebdb8601cd7ed2
Mariana Aguilera <marian5174@gmail.com>
2025-06-23 23:19:58 -0300

    Agrego comentarios que expliquen las clases

--- TECNICATURA_TercerSemestre/JAVA/Clase10/ListarPersonas/src/ListadoPersonasApp.java
+++ TECNICATURA_TercerSemestre/JAVA/Clase10/ListarPersonas/src/ListadoPersonasApp.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class ListadoPersonasApp {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in); //objeto scanner para leer la entrada del usuario
         //Definimos la lista fuera del ciclo while
-        List<Persona> personas = new ArrayList<>();
+        List<Persona> personas = new ArrayList<>(); //creamos la lista para almacenar objetos de tipo Persona
         //Empezamos el menu
-        var salir = false;
+        var salir = false; //para salir del menú
         while (!salir){
-            mostrarMenu();
+            mostrarMenu(); //mostramos el menú al usuario
             try{
-                salir = ejecutarOperacion(entrada, personas);
+                salir = ejecutarOperacion(entrada, personas); //se ejecuta la opción elegida y actualiza si debe salir
             } catch (Exception e){
-                System.out.println("Ocurrió un error: "+e.getMessage());
+                System.out.println("Ocurrió un error: "+e.getMessage()); //captura cualquier error del sistema
             }
             System.out.println();
         }//fin ciclo while
-    }//fin método main
+    } //fin método main
 
     private static void mostrarMenu(){
-        //mostramos las opciones
+        //mostramos las opciones al usuario
         System.out.print("""
                 ******* Listado de Personas *******
                 1. Agregar
@@ -31,35 +31,36 @@ public class ListadoPersonasApp {
         System.out.print("Seleccione una opción: ");
     }//fin método mostrar menú
 
+    // método que ejecuta la operación elegida por el usuario
     private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas){
-        var opcion = Integer.parseInt(entrada.nextLine());
-        var salir = false;
+        var opcion = Integer.parseInt(entrada.nextLine()); //lee la opción ingresada y la convierte a entero
+        var salir = false; //variable local para saber si salir del programa
         //Revisamos la opción digitada a través de un switch
         switch (opcion){
             case 1 -> { //agregar una persona a la lista
                 System.out.print("Ingrese el nombre: ");
-                var nombre = entrada.nextLine();
+                var nombre = entrada.nextLine(); //lee el nombre
                 System.out.print("Ingrese el teléfono: ");
-                var tel = entrada.nextLine();
+                var tel = entrada.nextLine(); //lee el teléfono
                 System.out.print("Ingrese el correo: ");
-                var email = entrada.nextLine();
+                var email = entrada.nextLine(); //lee el correo
                 //creamos el objeto persona
                 var persona = new Persona(nombre, tel, email);
                 //agregamos la persona a la lista
-                personas.add(persona);
+                personas.add(persona); //agrega el objeto Persona a la lista
                 System.out.println("La lista tiene: "+personas.size()+" elementos");
             }//fin caso 1
-            case 2 -> { //listar a las personas
+            case 2 -> { //listar a las personas registradas
                 System.out.println("Listado de personas: ");
                 //mejoras con lambda y el método de referencia
                 //personas.forEach((persona)-> System.out.println(persona));
-                personas.forEach(System.out::println);
+                personas.forEach(System.out::println); //imprime cada persona utilizando método de referencia
             }//fin caso 2
             case 3 -> { //salir del ciclo
                 System.out.println("Hasta pronto...");
                 salir = true;
             } //fin caso 3
-            default -> System.out.println("Opción incorrecta: "+opcion);
+            default -> System.out.println("Opción incorrecta: "+opcion); //mensaje por si se agrega una opción incorrecta
         } //fin del switch
         return salir;
     }//fin método ejecutarOperación
--- TECNICATURA_TercerSemestre/JAVA/Clase10/ListarPersonas/src/Persona.java
+++ TECNICATURA_TercerSemestre/JAVA/Clase10/ListarPersonas/src/Persona.java
@@ -1,55 +1,58 @@
 public class Persona {
-    private int id;
-    private String nombre;
-    private String tel;
-    private String email;
+    //atributos de la claase
+    private int id; //id de cada persona
+    private String nombre; //nombre de la persona
+    private String tel; //teléfono de cada persona
+    private String email; //correo de la persona
+    //atributo estático compartido para todas las instancias de la clase
     private static int numeroPersonas = 0;
 
     //Constructor vacío
     public Persona(){
-        this.id = ++Persona.numeroPersonas;
+        this.id = ++Persona.numeroPersonas; //se asigna el nuevo ID autoincrementando a la persona
     }
 
     //Constructor con parámetros(sobrecarga de constructores)
     public Persona(String nombre, String tel,String email){
-        this();
+        this(); //aca llama al constructor vacío para asignar el id automáticamente
         this.nombre = nombre;
         this.tel = tel;
         this.email = email;
     }
 
+    //Métodos getters y setters para acceder y modificar los atributos privados
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
-        this.id = id;
+        this.id = id; //devuelve el ID de la persona
     }
 
     public String getNombre() {
-        return nombre;
+        return nombre; //devuelve el nombre
     }
 
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        this.nombre = nombre; //nos permite modificar el nombre
     }
 
     public String getTel() {
-        return tel;
+        return tel; //devuelve el tel
     }
 
     public void setTel(String tel) {
-        this.tel = tel;
+        this.tel = tel; //permite modificcsr el tel
     }
 
     public String getEmail() {
-        return email;
+        return email; //devuelve el mail
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        this.email = email; //nos deja modificar el email
     }
-
+    //este método nos permite mostrar un objeto Persona como texto
     @Override
     public String toString() {
         return "Persona{" +
@@ -59,10 +62,10 @@ public class Persona {
                 ", email='" + email + '\'' +
                 '}';
     }
-
+    //Método main: para probar la clase de forma independiente
     public static void main(String[] args) {
-        Persona persona1 = new Persona("Juan Pérez", "15687650", "jperez@hotmail.com");
-        System.out.println(persona1);
+        Persona persona1 = new Persona("Juan Pérez", "15687650", "jperez@hotmail.com"); //crea una nueva persona
+        System.out.println(persona1); //imprime los datos de la persona
     }
 
 }
